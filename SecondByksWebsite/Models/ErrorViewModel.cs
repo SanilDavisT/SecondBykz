3f13864495e724a3fa0fc509d48912bc6e14b9b9
Sanil Davis <sanil.davis@speehive.com>
2022-02-13 16:45:33 +0530

    Add project files.

---
+++ SecondByksWebsite/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SecondByksWebsite.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
